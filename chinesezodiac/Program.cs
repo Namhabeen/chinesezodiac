2ef705d51288658d80fdcaf8c41bc4023a9fb216
South_under_been <s2018s27@e-mirim.hs.kr>
2020-05-06 23:45:13 +0900

    초기 커밋.

---
+++ chinesezodiac/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chinesezodiac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("태어난 년도를 입력하세요: ");
+            var input = int.Parse(Console.ReadLine());
+
+            switch (input % 12)
+            {
+                case 0:
+                    Console.WriteLine("원숭이띠");
+                    break;
+                case 1:
+                    Console.WriteLine("닭띠");
+                    break;
+                case 2:
+                    Console.WriteLine("개띠");
+                    break;
+                case 3:
+                    Console.WriteLine("돼지띠");
+                    break;
+                case 4:
+                    Console.WriteLine("쥐띠");
+                    break;
+                case 5:
+                    Console.WriteLine("소띠");
+                    break;
+                case 6:
+                    Console.WriteLine("호랑이띠");
+                    break;
+                case 7:
+                    Console.WriteLine("토끼띠");
+                    break;
+                case 8:
+                    Console.WriteLine("용띠");
+                    break;
+                case 9:
+                    Console.WriteLine("뱀띠");
+                    break;
+                case 10:
+                    Console.WriteLine("말띠");
+                    break;
+                case 11:
+                    Console.WriteLine("양띠");
+                    break;
+            }
+
+        }
+    }
+}
